bf8b6b07d64c14b4ca355537fb2fd4b85f227a54
woohyuk <7dngur7@naver.com>
2023-11-18 22:41:16 +0900

    move to naver map ㅎㅎ

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://naver.jfrog.io/artifactory/maven/")
+
+    }
+}
+
+rootProject.name = "SsainFe_Real"
+include(":app")
+ 
