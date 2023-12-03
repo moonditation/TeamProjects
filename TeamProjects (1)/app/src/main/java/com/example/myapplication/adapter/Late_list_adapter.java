37f30a9b159b479e450fa40e9e469d20cfe09ee8
woohyuk <7dngur7@naver.com>
2023-12-03 19:37:27 +0900

    지도 병합 완료

---
+++ TeamProjects (1)/app/src/main/java/com/example/myapplication/adapter/Late_list_adapter.java
@@ -0,0 +1,70 @@
+package com.example.myapplication.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.R;
+import com.example.myapplication.databinding.ItemActivePromBinding;
+import com.example.myapplication.databinding.ItemLateListBinding;
+import com.example.myapplication.late_memory.late_list_click;
+import com.example.myapplication.make_prom.received_promise_info;
+
+import java.util.List;
+
+public class Late_list_adapter extends RecyclerView.Adapter<Late_list_adapter.LateListViewHolder>{
+
+    private List<Promise> promiseList;
+
+    public Late_list_adapter(List<Promise> promiseList) {
+        this.promiseList = promiseList;
+    }
+
+    @Override
+    public Late_list_adapter.LateListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        ItemLateListBinding binding = ItemLateListBinding.inflate(LayoutInflater.from(
+                        parent.getContext()),
+                parent,
+                false);
+        return new Late_list_adapter.LateListViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(Late_list_adapter.LateListViewHolder holder, int position) {
+        Promise name = promiseList.get(position);
+        holder.bind(name);
+    }
+
+    @Override
+    public int getItemCount() {
+        return promiseList.size();
+    }
+
+    public class LateListViewHolder extends RecyclerView.ViewHolder {
+        private ItemLateListBinding binding;
+
+        private LateListViewHolder(ItemLateListBinding binding){
+            super(binding.getRoot());
+            this.binding = binding;
+
+            binding.lateProm.setOnClickListener(view -> {
+                int position = getAdapterPosition();
+                //이 position으로 id 파악해서 자료 뽑을거임
+                Promise promise = promiseList.get(position);
+                //처리해야하는 클릭이벤트.
+                FragmentManager fragmentManager = ((AppCompatActivity) view.getContext()).getSupportFragmentManager();
+                late_list_click late_list_click = new late_list_click();
+                fragmentManager.beginTransaction()
+                        .add(R.id.frame_layout, late_list_click)
+                        .addToBackStack(null)
+                        .commit();
+            });
+        }
+        private void bind(Promise text){binding.lateProm.setText(text.getPromiseName());}
+    }
+}
+
---
+++ TeamProjects (1)/app/src/main/java/com/example/myapplication/list_prom/waiting_promise.java
@@ -0,0 +1,64 @@
+package com.example.myapplication.list_prom;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myapplication.R;
+import com.example.myapplication.adapter.Prom_completed_adapter;
+import com.example.myapplication.adapter.Prom_waiting_adapter;
+import com.example.myapplication.adapter.Promise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class waiting_promise extends Fragment {
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_waiting_promise, container, false);
+
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // 리사이클러뷰 설정
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view_wait_prom);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        // 어댑터와 데이터 연결
+        List<Promise> dataList = generateData(); // 데이터 생성
+        Prom_waiting_adapter adapter = new Prom_waiting_adapter(dataList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    //다른 약속도 똑같이 정리하긴 할텐데, 리스트에 담는 과정을 정리하면 된다.
+    //사실 추가되더라고 크게 바뀔 것은 없다.
+    //특정 조건들을 통해서 도큐먼트들을 얻어와서, promise 에 넣어준다.
+    //promise class 의 생성자는 바꿔줘야겠지.
+    //그리고 게터 세터만 바꿔주면 될듯.
+    private List<Promise> generateData() {
+        List<Promise> dataList = new ArrayList<>();
+        // 데이터를 원하는대로 추가
+//        dataList.add(new Promise("이종민 1"));
+//        dataList.add(new Promise("이종민 2"));
+//        dataList.add(new Promise("이종민 3"));
+//        dataList.add(new Promise("이종민 4"));
+//        dataList.add(new Promise("이종민 5"));
+//        dataList.add(new Promise("이종민 6"));
+        // ...
+        return dataList;
+    }
+}
